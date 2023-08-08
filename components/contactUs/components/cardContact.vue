77682e82ee9ebe1d4b111851ffa9f1d32b8a3a9a
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2023-08-08 18:01:35 -0500

    add section contacus

--- components/contactUs/components/cardContact.vue
+++ components/contactUs/components/cardContact.vue
@@ -1,20 +1,25 @@
 <template>
   <div>
     <div
-      class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 text-center"
+      class="p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 text-center"
     >
-     <img/>
-      <a href="#">
-        <h5
-          class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white"
-        >
-          Need a help in Claim?
-        </h5>
-      </a>
+      <img :src="imageUrl" :alt="title" class="w-10 h-10 mx-auto mb-3" />
+      <h5
+        class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white"
+      >
+        {{ title }}
+      </h5>
       <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
-        Go to this step by step guideline process on how to certify for your
-        weekly benefits:
+        {{ data }}
       </p>
     </div>
   </div>
 </template>
+
+<script setup>
+defineProps({
+  title: String,
+  data: String,
+  imageUrl: String,
+});
+</script>
--- components/contactUs/components/sections/contactSection.vue
+++ components/contactUs/components/sections/contactSection.vue
@@ -2,10 +2,22 @@
   <div>
     <div class="section-container container mx-auto">
       <div class="text-4xl font-bold text-center mb-6">Contáctanos</div>
-      <div class="columns-3 ">
-        <ContactUsComponentsCardContact />
-        <ContactUsComponentsCardContact />
-        <ContactUsComponentsCardContact />
+      <div class="sm:columns-3 space-y-4 px-6 sm:space-x-6 sm:px-14">
+        <ContactUsComponentsCardContact
+          title="Encuentra nuestras oficinas"
+          data="Iquitos, calle lorem ipsus #2123"
+          imageUrl="/assets/images/svg/LocationOn.png"
+        />
+        <ContactUsComponentsCardContact
+          title="Contáctanos al telefono"
+          data="+51 965 620 970"
+          imageUrl="/assets/images/svg/Call.png"
+        />
+        <ContactUsComponentsCardContact
+          title="Envíanos un correo"
+          data="patrocinio@ficainternacional.com"
+          imageUrl="/assets/images/svg/Email.png"
+        />
       </div>
     </div>
   </div>
