3ff5586758bc6650b45698b959fda6db11afc7d3
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2023-08-08 12:16:13 -0500

    add section trajectory

--- components/about/components/Sections/moreHistorySection.vue
+++ components/about/components/Sections/moreHistorySection.vue
@@ -14,66 +14,31 @@
           <img
             src="/assets/images/trayectoria_fica.jpg"
             alt="conferencia"
-            class="p-6"
+            class="p-10"
           />
         </div>
         <div class="col-span-4 sm:col-span-6">
           <div>
             <ol class="relative border-l border-gray-200 dark:border-gray-700">
-              <li class="mb-10 ml-4">
+              <li
+                v-for="(item, index) in timelineItems"
+                :key="index"
+                class="mb-8 ml-4"
+              >
                 <div
                   class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
                 ></div>
                 <time
                   class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-                  >February 2022</time
+                  >{{ item.year }}</time
                 >
                 <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                  Application UI code in Tailwind CSS
+                  {{ item.title }}
                 </h3>
                 <p
                   class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"
                 >
-                  Get access to over 20+ pages including a dashboard layout,
-                  charts, kanban board, calendar, and pre-order E-commerce &
-                  Marketing pages.
-                </p>
-              </li>
-              <li class="mb-10 ml-4">
-                <div
-                  class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
-                ></div>
-                <time
-                  class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-                  >March 2022</time
-                >
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                  Marketing UI design in Figma
-                </h3>
-                <p
-                  class="text-base font-normal text-gray-500 dark:text-gray-400"
-                >
-                  All of the pages and components are first designed in Figma
-                  and we keep a parity between the two versions even as we
-                  update the project.
-                </p>
-              </li>
-              <li class="ml-4">
-                <div
-                  class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
-                ></div>
-                <time
-                  class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-                  >April 2022</time
-                >
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                  E-Commerce UI code in Tailwind CSS
-                </h3>
-                <p
-                  class="text-base font-normal text-gray-500 dark:text-gray-400"
-                >
-                  Get started with dozens of web components and interactive
-                  elements built on top of Tailwind CSS.
+                  {{ item.description }}
                 </p>
               </li>
             </ol>
@@ -89,3 +54,32 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      timelineItems: [
+        {
+          year: "February 1979",
+          title: "Comienzos del fica",
+          description:
+            "Nace el Festival Internacional de la Canción de la Amazonía (FICA), despertando la pasión de los amantes de la música en Loreto.",
+        },
+        {
+          year: "March 1984",
+          title: "Renombre del FICA",
+          description:
+            "Tras superar las expectativas del público, el FICA se consolida como un evento de renombre en la región amazónica.",
+        },
+        {
+          year: "April 1989",
+          title: "Resurgimiento del FICA",
+          description:
+            "El FICA experimenta un resurgimiento, cautivando nuevamente a la comunidad iquiteña y reafirmando su posición como un éxito rotundo.",
+        },
+      ],
+    };
+  },
+};
+</script>
