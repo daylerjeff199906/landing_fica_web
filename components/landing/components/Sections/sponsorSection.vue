c60cf7b4b132f845d027bcf5ded48b2a89a44928
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2023-08-15 23:28:57 -0500

    add section sponsors

--- components/landing/components/Sections/sponsorSection.vue
+++ components/landing/components/Sections/sponsorSection.vue
@@ -3,7 +3,7 @@
     <div class="section-container container mx-auto">
       <div class="mb-6 flex justify-between items-center">
         <h1 class="text-2xl font-roboto font-bold">Socios y auspiciadores</h1>
-        <GeneralButtonTextGeneral label="Ver todos" />
+        <GeneralButtonTextGeneral label="Ver todos" link-url="/sponsors" />
       </div>
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2">
         <img src="/assets/images/logoEmpresa.jpg" alt="" class="p-16" />
