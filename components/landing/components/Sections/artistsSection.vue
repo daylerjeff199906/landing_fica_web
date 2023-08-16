9aebbd52aeb0b92a7af98adbc500b7f7156317f9
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2023-08-15 22:35:22 -0500

    add artist view

--- components/landing/components/Sections/artistsSection.vue
+++ components/landing/components/Sections/artistsSection.vue
@@ -4,6 +4,7 @@
       <h1 class="text-3xl font-roboto font-bold">Artistas invitados</h1>
       <GeneralButtonTextGeneral 
       label="Ver todos los artistas"
+      link-url="/events/singers"
       />
     </div>
     <div class="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-4">
